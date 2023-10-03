d7081752248b98e38bfafc06648c1f29f25a168c
LinkCFF <cfierro@unal.edu.co>
2023-10-03 02:15:48 -0500

    ajustes para Calendar

--- src/controllers/calendar.Controllers.ts
+++ src/controllers/calendar.Controllers.ts
@@ -1,20 +1,23 @@
-import { Request, Response, NextFunction } from "express";
-import msRoutes from "../config/msRoutes";
+import { Request, Response, NextFunction } from 'express'
+// import msRoutes from '../config/msRoutes'
+import IndexEvents from '../services/calendar/IndexEvents.service'
 
 class calendarController {
-    private static apiRoute = msRoutes.calendar_ms
-    // basic requests for events
+    // private static apiRoute = msRoutes.calendar_ms
+    // // basic requests for events
     
-    public async createEvent(req: Request, res: Response, _next: NextFunction): Promise<void> {
+    // public async createEvent(req: Request, res: Response, _next: NextFunction): Promise<void> {
+    // }
+    // public async updateEvent(req: Request, res: Response, _next: NextFunction): Promise<void> {
+    // }
+    public async indexEvent(_req: Request, res: Response, _next: NextFunction): Promise<void> {
+        const event = await IndexEvents.IndexEvents()
+        res.status(event.status).json(event.answer)
     }
-    public async updateEvent(req: Request, res: Response, _next: NextFunction): Promise<void> {
-    }
-    public async indexEvent(req: Request, res: Response, _next: NextFunction): Promise<void> {
-    }
-    public async showSingleEvent(req: Request, res: Response, _next: NextFunction): Promise<void> {
-    }
-    public async destroyEvent(req: Request, res: Response, _next: NextFunction): Promise<void> {
-    }
+    // public async showSingleEvent(req: Request, res: Response, _next: NextFunction): Promise<void> {
+    // }
+    // public async destroyEvent(req: Request, res: Response, _next: NextFunction): Promise<void> {
+    // }
 }
 export default new calendarController();
 
--- src/routes/calendar.routes.ts
+++ src/routes/calendar.routes.ts
@@ -6,16 +6,23 @@ import calendarController from '../controllers/calendar.Controllers'
 router.get('/ping', (_req, res) => {
   res.send(`pong from calendar`)
 })
-// create event
-router.post('/events', calendarController.createEvent);
-// update event info
-router.put('/events/:id', calendarController.updateEvent);
+// // create event
+// router.post('/events', calendarController.createEvent);
+// // update event info
+// router.put('/events/:id', calendarController.updateEvent);
 // get all events
+// create pet
+router.post('')
+// update pet info by id
+router.put('')
 router.get('', calendarController.indexEvent);
-// get event by id
-router.get('/:id', calendarController.showSingleEvent);
-// delete event by id
-router.delete('/:id', calendarController.destroyEvent);
-
+// // get event by id
+// router.get('/:id', calendarController.showSingleEvent);
+// // delete event by id
+// router.delete('/:id', calendarController.destroyEvent);
+// get pet by id
+router.get('/:id')
+// delete pet by id
+router.delete('')
 export const CalendarRoutes: Router = router
 
